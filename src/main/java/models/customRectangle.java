00251143cb4c635ac858bb37241913fb953d86e4
Marc-Antoine <marc.ant.caron@gmail.com>
2020-01-25 17:36:59 -0500

    Added Drawable interface

--- src/main/java/models/customRectangle.java
+++ src/main/java/models/customRectangle.java
@@ -6,11 +6,15 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import ddraw4US.customSerializable;
+import ddraw4US.Drawable;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.util.Pair;
 
-public class customRectangle extends Rectangle implements customSerializable {
+public class customRectangle extends Rectangle implements customSerializable, Drawable {
 
 	public customRectangle() {
 		// TODO Auto-generated constructor stub
@@ -49,6 +53,20 @@ public class customRectangle extends Rectangle implements customSerializable {
 		oos.writeObject(this.getHeight());
 		
 		shapeWrite(oos, this);
+	}
+	
+	public Shape draw(Pair<Double, Double> startPositionXY, Pair<Double, Double> finalPositionXY) {
+		
+		double startX = startPositionXY.getKey();
+		double startY = startPositionXY.getValue();
+		
+		double finalX = finalPositionXY.getKey();
+		double finalY = finalPositionXY.getValue();
+		
+		double width = Math.max(finalX, startX) - Math.min(finalX, startX);
+		double height = Math.max(finalY, startY) - Math.min(finalY, startY);
+		
+		return new CustomRectangle(startX, startY, width, height);
 	}
 
 }
