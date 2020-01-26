58b3b103041b30e447ead33009e7309b19ca6e2e
Yannis <bouy1920@usherbrooke.ca>
2020-01-26 19:05:43 +0100

    EditDraw

---
+++ src/main/java/models/EditDraw.java
@@ -0,0 +1,37 @@
+package models;
+
+import javafx.scene.shape.Shape;
+
+public class EditDraw {
+	enum EditMode{
+		LINE,
+		ARROW,
+		RECTANGLE,
+		CIRCLE,
+		SELECTION
+	}
+	
+	private EditMode editMode;
+	private Shape actualShape;
+	
+	public EditMode getEditMode() {
+		return editMode;
+	}
+
+	public void setEditMode(EditMode editMode) {
+		this.editMode = editMode;
+	}
+
+	public Shape getActualShape() {
+		return actualShape;
+	}
+
+	public void setActualShape(Shape actualShape) {
+		this.actualShape = actualShape;
+	}
+
+	public EditDraw() {
+		this.actualShape = null;
+		this.editMode = EditMode.SELECTION;
+	}
+}
