0fa8b08d5a846566381520b9f7dc972a44ee96f6
Yannis <bouy1920@usherbrooke.ca>
2020-01-26 21:36:22 +0100

    Lien des controllers PC et PF avec leur view

--- src/main/java/ddraw4US/MainApp.java
+++ src/main/java/ddraw4US/MainApp.java
@@ -10,7 +10,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import view.ColorPaletteController;
 
 public class MainApp extends Application {
 
